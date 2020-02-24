987c22d8917dfd634c7682ca944a63bba80b103d
CapitalistPastor <59976408+CapitalistPastor@users.noreply.github.com>
2020-02-23 22:36:11 -0500

    Add files via upload

---
+++ Maia_problem2/src/SavingsAccount.java
@@ -0,0 +1,18 @@
+
+public class SavingsAccount {
+	public static void main(String args[]) {
+	static int annualInterestRate;
+	
+	}
+
+	private int savingsBalance() {
+		
+	}
+	
+	public static int calculateMonthlyInterest() {
+		int result;
+		result = savingsBalance()*annualInterestRate/12;
+		return result;
+	}
+
+}
